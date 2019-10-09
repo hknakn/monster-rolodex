5cd69842aa12b708dd8eda8c4ec2fc92a8ac0ea0
hknakn <cehakanakin@gmail.com>
2019-10-10 00:22:54 +0300

    Updates

---
+++ precache-manifest.a3de23fbbc158422b93dcbc1dfa68d59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408eee8d55d63089d40a0c0eabd94532",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d9f7337c870cecc81036",
+    "url": "/monster-rolodex/static/css/main.7c0abb0e.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monster-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "d9f7337c870cecc81036",
+    "url": "/monster-rolodex/static/js/main.fc65608c.chunk.js"
+  },
+  {
+    "revision": "e1d991a2b9a3476b0dce",
+    "url": "/monster-rolodex/static/js/runtime-main.df1bd6b7.js"
+  }
+]);
